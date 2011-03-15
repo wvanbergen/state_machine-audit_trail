bbe9f295946fa02aaf75e0a3099a14a834aa919c
Jesse Storimer <jstorimer@gmail.com>
2011-03-15 09:09:24 -0400

    typo

--- lib/state_machine/audit_trail/active_record.rb
+++ lib/state_machine/audit_trail/active_record.rb
@@ -1,7 +1,7 @@
 class StateMachine::AuditTrail::ActiveRecord < StateMachine::AuditTrail::Base
   def log(object, event, from, to, timestamp = Time.now)
     # Let ActiveRecord manage the timestamp for us so it does the 
-    # right thing width regards to timezones.
+    # right thing with regards to timezones.
     transition_class.create(foreign_key_field(object) => object.id, :event => event, :from => from, :to => to)
   end
 
