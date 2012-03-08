eb03c7f94c92433c335c74ba43c6c034ce1cdf9f
August Jaenicke <august@carbonfive.com>
2012-03-08 08:57:50 -0800

    First pass at allowing additional context to be captured in the transitions

--- lib/state_machine/audit_trail/backend/active_record.rb
+++ lib/state_machine/audit_trail/backend/active_record.rb
@@ -3,7 +3,11 @@ class StateMachine::AuditTrail::Backend::ActiveRecord < StateMachine::AuditTrail
   def log(object, event, from, to, timestamp = Time.now)
     # Let ActiveRecord manage the timestamp for us so it does the 
     # right thing with regards to timezones.
-    transition_class.create(foreign_key_field(object) => object.id, :event => event, :from => from, :to => to)
+    params = {foreign_key_field(object) => object.id, :event => event, :from => from, :to => to}
+
+    filtered = (transition_class.attribute_names.collect { |key| key.to_sym } - params.keys - [:id, :created_at])
+    filtered.each {|param| params[param] = object.send(param)}
+    transition_class.create(params)
   end
 
   def foreign_key_field(object)
--- spec/state_machine/active_record_spec.rb
+++ spec/state_machine/active_record_spec.rb
@@ -2,15 +2,15 @@ require 'spec_helper'
 require 'helpers/active_record'
 
 describe StateMachine::AuditTrail::Backend::ActiveRecord do
-  
+
   it "should create an ActiveRecord backend" do
     backend = StateMachine::AuditTrail::Backend.create_for_transition_class(ActiveRecordTestModelStateTransition)
     backend.should be_instance_of(StateMachine::AuditTrail::Backend::ActiveRecord)
-  end  
-  
+  end
+
   context 'on an object with a single state machine' do
     let!(:state_machine) { ActiveRecordTestModel.create! }
-    
+
     it "should log an event with all fields set correctly" do
       state_machine.start!
       last_transition = ActiveRecordTestModelStateTransition.where(:active_record_test_model_id => state_machine.id).last
@@ -18,21 +18,36 @@ describe StateMachine::AuditTrail::Backend::ActiveRecord do
       last_transition.event.to_s.should == 'start'
       last_transition.from.should == 'waiting'
       last_transition.to.should == 'started'
+      #last_transition.context.should_not be_nil
       last_transition.created_at.should be_within(10.seconds).of(Time.now.utc)
     end
-    
+
     it "should log multiple events" do
       lambda { state_machine.start && state_machine.stop && state_machine.start }.should change(ActiveRecordTestModelStateTransition, :count).by(3)
     end
-    
+
     it "should do nothing when the transition is not exectuted successfully" do
       lambda { state_machine.stop }.should_not change(ActiveRecordTestModelStateTransition, :count)
     end
   end
-  
+
+  context 'on an object with a single state machine that wants to log context' do
+    before do
+      backend = StateMachine::AuditTrail::Backend.create_for_transition_class(ActiveRecordTestModelWithContextStateTransition)
+    end
+
+    let!(:state_machine) { ActiveRecordTestModelWithContext.create! }
+
+    it "should log an event with all fields set correctly" do
+      state_machine.start!
+      last_transition = ActiveRecordTestModelWithContextStateTransition.where(:active_record_test_model_with_context_id => state_machine.id).last
+      last_transition.context.should_not be_nil
+    end
+  end
+
   context 'on an object with multiple state machines' do
     let!(:state_machine) { ActiveRecordTestModelWithMultipleStateMachines.create! }
-    
+
     it "should log a state transition for the affected state machine" do
       lambda { state_machine.begin_first! }.should change(ActiveRecordTestModelWithMultipleStateMachinesFirstTransition, :count).by(1)
     end
@@ -41,15 +56,15 @@ describe StateMachine::AuditTrail::Backend::ActiveRecord do
       lambda { state_machine.begin_first! }.should_not change(ActiveRecordTestModelWithMultipleStateMachinesSecondTransition, :count)
     end
   end
-  
+
   context 'on an object with a state machine having an initial state' do
     let(:state_machine_class) { ActiveRecordTestModelWithMultipleStateMachines }
     let(:state_transition_class) { ActiveRecordTestModelWithMultipleStateMachinesFirstTransition }
-    
+
     it "should log a state transition for the inital state" do
       lambda { state_machine_class.create! }.should change(state_transition_class, :count).by(1)
     end
-    
+
     it "should only set the :to state for the initial transition" do
       state_machine_class.create!
       initial_transition = state_transition_class.last
@@ -59,11 +74,11 @@ describe StateMachine::AuditTrail::Backend::ActiveRecord do
       initial_transition.created_at.should be_within(10.seconds).of(Time.now.utc)
     end
   end
-  
+
   context 'on an object with a state machine not having an initial state' do
     let(:state_machine_class) { ActiveRecordTestModelWithMultipleStateMachines }
     let(:state_transition_class) { ActiveRecordTestModelWithMultipleStateMachinesSecondTransition }
-    
+
     it "should not log a transition when the object is created" do
       lambda { state_machine_class.create! }.should_not change(state_transition_class, :count)
     end
