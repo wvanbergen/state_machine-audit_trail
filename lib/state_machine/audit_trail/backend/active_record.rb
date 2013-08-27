2eeeb8e8e2563ec53f7f7d377cb92f1020ccd2cd
Justin Smestad <justin.smestad@gmail.com>
2013-08-27 14:47:34 -0600

    Update active_record.rb to use 'from_state' and 'to_state' avoiding reserved words
    These are reserved in some relational databases

--- lib/state_machine/audit_trail/backend/active_record.rb
+++ lib/state_machine/audit_trail/backend/active_record.rb
@@ -9,7 +9,7 @@ class StateMachine::AuditTrail::Backend::ActiveRecord < StateMachine::AuditTrail
   def log(object, event, from, to, timestamp = Time.now)
     # Let ActiveRecord manage the timestamp for us so it does the 
     # right thing with regards to timezones.
-    params = {foreign_key_field(object) => object.id, :event => event, :from => from, :to => to}
+    params = {foreign_key_field(object) => object.id, :event => event, :from_state => from, :to_state => to}
     params[self.context_to_log] = object.send(self.context_to_log) unless self.context_to_log.nil?
     transition_class.create(params)
   end
