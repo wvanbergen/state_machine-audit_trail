aa4f4b776b109e26ec7e0bf695ebb7e6b024045a
Nathan G. <nathan@ngriffith.com>
2013-12-09 19:46:33 -0500

    Use 1.8 syntax for :class_name

--- lib/state_machine/audit_trail/backend/active_record.rb
+++ lib/state_machine/audit_trail/backend/active_record.rb
@@ -5,7 +5,7 @@ class StateMachine::AuditTrail::Backend::ActiveRecord < StateMachine::AuditTrail
     self.context_to_log = context_to_log
     @association = transition_class.to_s.tableize.split('/').last.to_sym
     super transition_class
-    owner_class.has_many(@association, class_name: transition_class.to_s) unless owner_class.reflect_on_association(@association)
+    owner_class.has_many(@association, :class_name => transition_class.to_s) unless owner_class.reflect_on_association(@association)
   end
 
   def log(object, event, from, to, timestamp = Time.now)
