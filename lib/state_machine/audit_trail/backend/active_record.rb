02d1c4b580c501a23869d52aba6862900e1d9d5e
weyus <wes@redflag.com>
2013-12-27 16:49:25 -0600

    Store event as a string, not a YAML-serialized symbol.

--- lib/state_machine/audit_trail/backend/active_record.rb
+++ lib/state_machine/audit_trail/backend/active_record.rb
@@ -11,7 +11,7 @@ class StateMachine::AuditTrail::Backend::ActiveRecord < StateMachine::AuditTrail
   def log(object, event, from, to, timestamp = Time.now)
     # Let ActiveRecord manage the timestamp for us so it does the 
     # right thing with regards to timezones.
-    params = {:event => event, :from => from, :to => to}
+    params = {:event => event ? event.to_s : nil, :from => from, :to => to}
     [context_to_log].flatten(1).each { |context| params[context] = object.send(context) } unless self.context_to_log.nil?
 
     if object.new_record?
